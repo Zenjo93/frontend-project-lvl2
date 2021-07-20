2583a85ae3b520a5c89e6fbb134a9991bbf79abb
Zenjo <vl.brzn@gmail.com>
2021-07-20 16:55:23 +0300

    fix commander output

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 import gendiff from '../src/gendiffcli.js'
+import  { program } from 'commander';
+
+program.version('0.0.1');
+program
+.description('Compares two configuration files and shows a difference.')
+.parse(process.argv);
+  
 
 gendiff();
 
--- src/gendiffcli.js
+++ src/gendiffcli.js
@@ -2,11 +2,7 @@ import  { program } from 'commander';
 
 
 export default function () {
-  program.version('0.0.1');
-  program.description('Compares two configuration files and shows a difference.');
-  program.parse(process.argv);
-  
-  
+
 }
 
 
