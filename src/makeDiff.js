084610faa444d3ef898dcc9d6fedb242de9c4b59
Zenjo <vl.brzn@gmail.com>
2021-08-06 14:57:25 +0300

    refactor buildAST.js

--- src/makeDiff.js
+++ src/makeDiff.js
@@ -1,5 +1,5 @@
 import parse from './parsers.js';
-import { buildAST } from './buildAST.js';
+import buildAST from './buildAST.js';
 import { formatStylish } from './formatStylish.js';
 // TODO: Программа должна уметь работать как с относительными, так и абсолютными путями до файлов
 
@@ -8,12 +8,12 @@ export default (path1, path2) => {
 
   // Составление объекта с различиями
   const diffObject = buildAST(file1, file2);
-  // console.log(`DIFFOBJECT: ${JSON.stringify(diffObject, null, 2)}`);
+  console.log(`DIFFOBJECT NEW: ${JSON.stringify(diffObject, null, 2)}`);
 
   // Форматирование объекта (stylish) возвращает строку
   const formatedDiffTree = formatStylish(diffObject);
 
-  //console.log(`${'RESULT: ' + '\n'}${formatedDiffTree}`);
+  // console.log(`${'RESULT: ' + '\n'}${formatedDiffTree}`);
 
   // console.log('result:' + result);
   return formatedDiffTree;
