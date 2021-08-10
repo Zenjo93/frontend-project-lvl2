572e9beecea80c9cbfba0338e0b0ccf75f2f4343
Zenjo <vl.brzn@gmail.com>
2021-08-10 20:33:55 +0300

    fix format Stylish

--- src/formatStylish.js
+++ src/formatStylish.js
@@ -1,64 +1,68 @@
 import _ from 'lodash';
 
-const getChildren = (tree) => {
-  if (Array.isArray(tree)) {
-    return tree;
-  }
-  return tree.children;
+const INDENT_COUNT = 2;
+
+const SIGNS = {
+  deleted: ' - ',
+  added: ' + ',
+  unchanged: ' ',
 };
 
+// const getIndent = (depth = 1, size = 'half') => {
+//
+//   if (size === 'full') {
+//     return ' '.repeat(INDENT_COUNT * depth);
+//   }
+//   return ' '.repeat(INDENT_COUNT * depth - 2);
+//
+// };
+//
 
-const buildString = (node, currentIndent) => {
-  switch (node.status) {
-    case 'deleted': //
-      return `${currentIndent} - ${node.name}: ${node.value}`;
-    case 'added':
-      return `${currentIndent} + ${node.name}: ${node.value}`;
-    case 'changed':
-      return [
-        `${currentIndent} - ${node.name}: ${node.value[0]}`,
-        `${currentIndent} + ${node.name}: ${node.value[1]}`].join('\n');
-    case 'unchanged':
-      return `${currentIndent}   ${node.name}: ${node.value}`;
+const getIndent = (depth = 1) => {
+  // если есть знак + 2 без знака * 2
+  const indentSize = depth * INDENT_COUNT;
+  const currentIndent = ' '.repeat(indentSize);
 
-    default: return new Error('Unknown status');
+  return currentIndent;
+}
+
+
+const buildValue = (value, depth) => {
+  if (_.isObject(value)) {
+    const keys = _.keys(value);
+    const lines = keys.map((key) => {
+      return `${getIndent(depth + 2, 'full')}${SIGNS.unchanged}${key}: ${buildValue(value[key], depth + 2)}`;
+    });
+    return ['{', ...lines, `${getIndent(depth)} }`,
+    ].join('\n');
+  } else {
+    return value;
   }
 };
+
 
 const formatStylish = (tree, depth = 1) => {
-  // TODO: убрать итер
-  const iter = (node, depth) => {
-    // подсчет отступов в отдельную функцию в зависимости от глубины
-    const spacesCount = 2;
-    const indentSize = depth * spacesCount;
-    const currentIndent = ' '.repeat(indentSize);
-    const bracketIndent = ' '.repeat(indentSize - spacesCount);
 
-    if (_.has(node, 'value')) {
-      return buildString(node, currentIndent);
+  const lines = tree.flatMap((node) => {
+    if (node.type === 'nested') {
+      return `${getIndent(depth + 1)}${SIGNS.unchanged}${node.name}: ${formatStylish(node.children, depth + 2)}`;
     }
-
-    const name = node.name;
-    const children = getChildren(node);
-
-    // проходим switch (в зависимости от типа ноды выбираем что с ней делать)
-    const lines = children.flatMap((child) => {
-
-      // console.log('child: ' + JSON.stringify(child, null, 2))
-      // return `${currentIndent}${child.name}: {${iter(child, depth + 1)}`;
-      return iter(child, depth + 1);
-
-    });
-
-    if (typeof name === 'undefined') {
-      return ['\n {', ...lines, `${bracketIndent}}`].join('\n');
+    if (node.type === 'deleted') {
+      return `${getIndent(depth)}${SIGNS.deleted}${node.name}: ${buildValue(node.value, depth + 1)}`;
     }
-    return [`${currentIndent}${name}: {`, ...lines, `${bracketIndent}}`].join('\n');
-
-  };
-
-  return iter(tree, 1);
+    if (node.type === 'added') {
+      return `${getIndent(depth)}${SIGNS.added}${node.name}: ${buildValue(node.value, depth + 1)}`;
+    }
+    if (node.type === 'changed') {
+      return [
+        `${getIndent(depth)}${SIGNS.deleted}${node.name}: ${buildValue(node.value[0], depth + 1)}`,
+        `${getIndent(depth)}${SIGNS.added}${node.name}: ${buildValue(node.value[1], depth + 1)}`,
+      ];
+    }
+    return `${getIndent(depth + 1)}${SIGNS.unchanged}${node.name}: ${buildValue(node.value, depth + 1)}`;
+  });
+  return ['{', ...lines, `${getIndent(depth - 1)} }`,
+  ].join('\n');
 };
 
 export default formatStylish;
-
