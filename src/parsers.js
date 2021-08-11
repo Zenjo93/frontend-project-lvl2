5d0d7c50176daf6f8445936c51a97219c3b342d5
Zenjo <vl.brzn@gmail.com>
2021-08-11 17:58:01 +0300

    fix parsers

--- src/parsers.js
+++ src/parsers.js
@@ -2,16 +2,25 @@ import * as fs from 'fs';
 import yaml from 'js-yaml';
 import path from 'path';
 
+const readFile = (filePath) => {
+  const fullPath = path.resolve(process.cwd(), filePath);
+  const data = fs.readFileSync(fullPath);
+  return data;
+};
+
 export default (path1, path2) => {
+  const file1 = readFile(path1);
+  const file2 = readFile(path2);
+
   const extName = path.extname(path1);
 
   if (extName === '.yml' || extName === '.yaml') {
-    const file1 = yaml.load(fs.readFileSync(path1));
-    const file2 = yaml.load(fs.readFileSync(path2));
-    return [file1, file2];
+    const data1 = yaml.load(file1);
+    const data2 = yaml.load(file2);
+    return [data1, data2];
   }
 
-  const file1 = JSON.parse(fs.readFileSync(path1));
-  const file2 = JSON.parse(fs.readFileSync(path2));
-  return [file1, file2];
+  const data1 = JSON.parse(file1);
+  const data2 = JSON.parse(file2);
+  return [data1, data2];
 };
