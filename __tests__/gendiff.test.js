2aec56f176c1069e25851c99512ddb7b9fc8b58a
Zenjo <vl.brzn@gmail.com>
2021-08-18 13:50:30 +0300

    refactor tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,16 +1,16 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import * as fs from 'fs';
-import genDiff from '../gendiff.js';
+import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const stylishFormat = fs.readFileSync(getFixturePath('stylish.txt'), 'utf-8');
-const plainFormat = fs.readFileSync(getFixturePath('plain.txt'), 'utf-8');
-const JSONFormat = fs.readFileSync(getFixturePath('jsonFormat.json'), 'utf-8');
+const stylishExpected = fs.readFileSync(getFixturePath('stylishExpected.txt'), 'utf-8');
+const plainExpected = fs.readFileSync(getFixturePath('plainExpected.txt'), 'utf-8');
+const jsonExpected = fs.readFileSync(getFixturePath('jsonExpected.json'), 'utf-8');
 
 const JSONPath1 = getFixturePath('file1.json');
 const JSONPath2 = getFixturePath('file2.json');
@@ -18,38 +18,35 @@ const JSONPath2 = getFixturePath('file2.json');
 const YmlPath1 = getFixturePath('file1.yml');
 const YmlPath2 = getFixturePath('file2.yaml');
 
-describe('Stylish formatting', () => {
-  test('JSON files', () => {
-    const actual = genDiff(JSONPath1, JSONPath2);
-    expect(actual).toEqual(stylishFormat);
-  });
+const cases = [
+  {
+    format: 'stylish',
+    expected: stylishExpected,
+  },
+  {
+    format: 'plain',
+    expected: plainExpected,
+  },
+  {
+    format: 'json',
+    expected: jsonExpected,
+  },
+];
 
-  test('YML files', () => {
-    const actual = genDiff(YmlPath1, YmlPath2);
-    expect(actual).toEqual(stylishFormat);
-  });
+test.each(cases)('JSON format $format', ({
+  format,
+  expected,
+}) => {
+  const actual = genDiff(JSONPath1, JSONPath2, format);
+  expect(actual)
+    .toEqual(expected);
 });
 
-describe('Plain formatting', () => {
-  test('JSON files', () => {
-    const actual = genDiff(JSONPath1, JSONPath2, 'plain');
-    expect(actual).toEqual(plainFormat);
-  });
-
-  test('YML files', () => {
-    const actual = genDiff(YmlPath1, YmlPath2, 'plain');
-    expect(actual).toEqual(plainFormat);
-  });
-});
-
-describe('JSON formatting', () => {
-  test('JSON files', () => {
-    const actual = genDiff(JSONPath1, JSONPath2, 'json');
-    expect(actual).toEqual(JSONFormat);
-  });
-
-  test('YML files', () => {
-    const actual = genDiff(YmlPath1, YmlPath2, 'json');
-    expect(actual).toEqual(JSONFormat);
-  });
+test.each(cases)('YML format $format', ({
+  format,
+  expected,
+}) => {
+  const actual = genDiff(YmlPath1, YmlPath2, format);
+  expect(actual)
+    .toEqual(expected);
 });
--- bin/cli.js
+++ bin/cli.js
@@ -1,16 +1,11 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import genDiff from '../gendiff.js';
+import genDiff from '../index.js';
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format', 'stylish');
-
-program
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<path1>, <path2>')
-  .action((path1, path2, options) => {
-    console.log(genDiff(path1, path2, options.format));
-  });
-
-program.parse();
+  .action((path1, path2, options) => console.log(genDiff(path1, path2, options.format)))
+  .parse();
