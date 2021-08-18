9e309723501d1ab5e6ca8b024798d16d5c802f77
Zenjo <vl.brzn@gmail.com>
2021-08-18 14:01:34 +0300

    fix parser

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+import path from 'path';
+import fs from 'fs';
 import parse from './src/parsers.js';
 import buildAST from './src/buildAST.js';
 import formatDiff from './src/formatters/index.js';
 
+const readFile = (filePath) => {
+  const fullPath = path.resolve(process.cwd(), filePath);
+  return fs.readFileSync(fullPath);
+};
+
 export default (path1, path2, formatName = 'stylish') => {
-  const [file1, file2] = parse(path1, path2);
-  const tree = buildAST(file1, file2);
+  const file1 = readFile(path1);
+  const file2 = readFile(path2);
 
-  const diff = formatDiff(tree, formatName);
+  const extFile1 = path.extname(path1);
+  const extFile2 = path.extname(path2);
 
-  return diff;
+  const data1 = parse(file1, extFile1);
+  const data2 = parse(file2, extFile2);
+
+  const tree = buildAST(data1, data2);
+
+  return formatDiff(tree, formatName);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,26 +1,9 @@
-import * as fs from 'fs';
 import yaml from 'js-yaml';
-import path from 'path';
 
-const readFile = (filePath) => {
-  const fullPath = path.resolve(process.cwd(), filePath);
-  const data = fs.readFileSync(fullPath);
-  return data;
-};
-
-export default (path1, path2) => {
-  const file1 = readFile(path1);
-  const file2 = readFile(path2);
-
-  const extName = path.extname(path1);
-
-  if (extName === '.yml' || extName === '.yaml') {
-    const data1 = yaml.load(file1);
-    const data2 = yaml.load(file2);
-    return [data1, data2];
+export default (data, extension) => {
+  if (extension === '.yml' || extension === '.yaml') {
+    return yaml.load(data);
   }
 
-  const data1 = JSON.parse(file1);
-  const data2 = JSON.parse(file2);
-  return [data1, data2];
+  return JSON.parse(data);
 };
