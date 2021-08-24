f48e23d016b24345e06f596d7604935095fc0b21
Zenjo <vl.brzn@gmail.com>
2021-08-24 10:24:14 +0300

    fixes 24/08

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,30 +1,54 @@
-const getValue = (value) => {
+import _ from 'lodash';
+
+const stringify = (value) => {
   if (typeof value === 'string') {
     return `'${value}'`;
   }
-  if (typeof value === 'object' && value !== null) {
+  if (_.isObject(value)) {
     return '[complex value]';
   }
   return value;
 };
 
+const mapping = {
+  deleted: (path, nodeName) => `Property '${path}${nodeName}' was removed`,
+  added: (path, nodeName, nodeValue) => `Property '${path}${nodeName}' was added with value: ${stringify(nodeValue)}`,
+  changed: (path, nodeName, nodeValue) => `Property '${path}${nodeName}' was updated. From ${stringify(nodeValue[0])} to ${stringify(nodeValue[1])}`,
+  unchanged: () => 'unchanged',
+};
+
 const formatPlain = (tree) => {
   const iter = (currentNode, path = '') => {
-    const lines = currentNode.filter((node) => node.type !== 'unchanged').flatMap((node) => {
+    const lines = currentNode.flatMap((node) => {
       if (node.type === 'nested') {
         return iter(node.children, `${path}${node.name}.`);
       }
-      if (node.type === 'deleted') {
-        return `Property '${path}${node.name}' was removed`;
-      }
-      if (node.type === 'added') {
-        return `Property '${path}${node.name}' was added with value: ${getValue(node.value)}`;
-      }
-      return `Property '${path}${node.name}' was updated. From ${getValue(node.value[0])} to ${getValue(node.value[1])}`;
+      return mapping[node.type](path, node.name, node.value);
     });
-    return [...lines].filter((el) => typeof el !== 'undefined').join('\n');
+    return [...lines].filter((el) => el !== 'unchanged')
+      .join('\n');
   };
   return iter(tree);
 };
 
 export default formatPlain;
+
+// const formatPlain = (tree) => {
+//   const iter = (currentNode, path = '') => {
+//     const lines = currentNode.filter((node) => node.type !== 'unchanged').flatMap((node) => {
+//       if (node.type === 'nested') {
+//         return iter(node.children, `${path}${node.name}.`);
+//       }
+//       if (node.type === 'deleted') {
+//         return `Property '${path}${node.name}' was removed`;
+//       }
+//       if (node.type === 'added') {
+//         return `Property '${path}${node.name}' was added with value: ${getValue(node.value)}`;
+//       }
+// eslint-disable-next-line max-len
+//       return `Property '${path}${node.name}' was updated. From ${getValue(node.value[0])} to ${getValue(node.value[1])}`;
+//     });
+//     return [...lines].filter((el) => typeof el !== 'undefined').join('\n');
+//   };
+//   return iter(tree);
+// };
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -8,44 +8,66 @@ const SIGNS = {
   unchanged: '',
 };
 
-const getIndent = (depth = 1) => {
+const getIndent = (depth) => {
   const indentSize = depth * INDENT_COUNT;
   const currentIndent = ' '.repeat(indentSize);
 
   return currentIndent;
 };
 
-const buildValue = (value, depth) => {
-  if (_.isObject(value)) {
-    const keys = _.keys(value);
-    const lines = keys.map((key) => `${getIndent(depth + 2)}${key}: ${buildValue(value[key], depth + 2)}`);
-    return ['{', ...lines, `${getIndent(depth)}}`,
-    ].join('\n');
+const stringify = (value, depth) => {
+  if (!_.isObject(value)) {
+    return value;
   }
-  return value;
+  const keys = _.keys(value);
+  const lines = keys.map((key) => `${getIndent(depth + 2)}${key}: ${stringify(value[key], depth + 2)}`);
+  return ['{', ...lines, `${getIndent(depth)}}`,
+  ].join('\n');
 };
 
-const stylish = (tree, depth = 1) => {
+const mapping = {
+  deleted: (name, value, depth) => `${getIndent(depth)}${SIGNS.deleted}${name}: ${stringify(value, depth + 1)}`,
+  added: (name, value, depth) => `${getIndent(depth)}${SIGNS.added}${name}: ${stringify(value, depth + 1)}`,
+  changed: (name, value, depth) => [
+    `${getIndent(depth)}${SIGNS.deleted}${name}: ${stringify(value[0], depth + 1)}`,
+    `${getIndent(depth)}${SIGNS.added}${name}: ${stringify(value[1], depth + 1)}`,
+  ],
+  unchanged: (name, value, depth) => `${getIndent(depth + 1)}${SIGNS.unchanged}${name}: ${stringify(value, depth + 1)}`,
+};
+
+const formatStylish = (tree, depth = 1) => {
   const lines = tree.flatMap((node) => {
     if (node.type === 'nested') {
-      return `${getIndent(depth + 1)}${node.name}: ${stylish(node.children, depth + 2)}`;
+      return `${getIndent(depth + 1)}${node.name}: ${formatStylish(node.children, depth + 2)}`;
     }
-    if (node.type === 'deleted') {
-      return `${getIndent(depth)}${SIGNS.deleted}${node.name}: ${buildValue(node.value, depth + 1)}`;
-    }
-    if (node.type === 'added') {
-      return `${getIndent(depth)}${SIGNS.added}${node.name}: ${buildValue(node.value, depth + 1)}`;
-    }
-    if (node.type === 'changed') {
-      return [
-        `${getIndent(depth)}${SIGNS.deleted}${node.name}: ${buildValue(node.value[0], depth + 1)}`,
-        `${getIndent(depth)}${SIGNS.added}${node.name}: ${buildValue(node.value[1], depth + 1)}`,
-      ];
-    }
-    return `${getIndent(depth + 1)}${SIGNS.unchanged}${node.name}: ${buildValue(node.value, depth + 1)}`;
+    return mapping[node.type](node.name, node.value, depth);
   });
   return ['{', ...lines, `${getIndent(depth - 1)}}`,
   ].join('\n');
 };
 
-export default stylish;
+export default formatStylish;
+
+
+// const formatStylish = (tree, depth = 1) => {
+//   const lines = tree.flatMap((node) => {
+//     if (node.type === 'nested') {
+//       return `${getIndent(depth + 1)}${node.name}: ${formatStylish(node.children, depth + 2)}`;
+//     }
+//     if (node.type === 'deleted') {
+//       return `${getIndent(depth)}${SIGNS.deleted}${node.name}: ${stringify(node.value, depth + 1)}`;
+//     }
+//     if (node.type === 'added') {
+//       return `${getIndent(depth)}${SIGNS.added}${node.name}: ${stringify(node.value, depth + 1)}`;
+//     }
+//     if (node.type === 'changed') {
+//       return [
+//         `${getIndent(depth)}${SIGNS.deleted}${node.name}: ${stringify(node.value[0], depth + 1)}`,
+//         `${getIndent(depth)}${SIGNS.added}${node.name}: ${stringify(node.value[1], depth + 1)}`,
+//       ];
+//     }
+//     return `${getIndent(depth + 1)}${SIGNS.unchanged}${node.name}: ${stringify(node.value, depth + 1)}`;
+//   });
+//   return ['{', ...lines, `${getIndent(depth - 1)}}`,
+//   ].join('\n');
+// };
